3398784105e2f6c7e7d231617d405748ec00c4df
arashhaji <56742302+arashhaji@users.noreply.github.com>
2019-12-11 22:07:13 -0500

    Added Style Css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
 import React from "react";
 import ImgCard from "./components/ImgCard.js";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+
+
+import styled from "styled-components";
+
+
+const H1 = styled.h1`
+  font-size: 65px;
+  max-witdth: 800px;
+  color: white;
+  font-family: The Wild Hammers;
+  background-color: black;
+  padding: 3%;
+  margin-top: 1%;
+  border: 1.5px solid white;
+  box-shadow: 5px 5px 5px slategray;
+  `
+
+  const Border = styled.p`
+    font-size: 14px;
+    padding:2%;
+    background-color: black;
+    color: white;
+    align-text: center;
+    border: .5px solid white;
+    box-shadow: 5px 5px 5px 5px slategray;
+    `;
+
+    
 
 
 function App() {
 return (
   <div className = "App">
-    <h1>Nasa Image of the Day</h1>
+    <H1>Nasa Image of the Day</H1>
    <ImgCard />
-    </div>	   
+   <div className = "border">
+     <Border>Copyright Nasa 2019 | All Rights Reserved</Border>
+   </div>
+    </div>   
 
-);
+  );
 }
 
 export default App;
--- src/components/ImgCard.js
+++ src/components/ImgCard.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import ImgInfo from "./ImgInfo";
 
+
+
 const ImgCard = () => {
     const [title, setTitle] = useState();
     const [date, setDate] = useState();
